f84afd7aa6d6b05d6bc60d2678a2b971812033a9
Max Pavlukhin <pavlukhinm@gmail.com>
2019-04-06 22:46:44 +0300

    Add project structure files

---
+++ NaiveBayes.py
@@ -0,0 +1,21 @@
+"""имплементация наивного байесовского классификатора"""
+class NaiveBayes:
+    def __init__(self):
+        pass
+
+    def fit(self, data: list, target: list):
+        """
+
+        :param data: массив документов, каждый документ - объект типа str
+        :param target: массив меток объектов
+        :return:
+        """
+        pass
+
+    def predict(self, data: list):
+        """
+
+        :param data: массив документов, для каждого из которых нужно предсказать метку
+        :return:
+        """
+        pass
